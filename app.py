7e3adff3f08142a5fe56ba974fec502e83c97e23
ConnorOMalley <william.omalley@umconnect.umt.edu>
2023-12-18 16:41:30 -0700

    updates

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import tensorflow as tf 
 import os 
+
+# Avoid out of memory errors by setting GPU memory consumption growth
+gpus = tf.config.experimental.list_physical_devices('CPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+print(gpus)
