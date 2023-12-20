fcd4372a736782d6345dd5a991145a4dd790190d
ConnorOMalley <william.omalley@umconnect.umt.edu>
2023-12-19 18:52:20 -0700

    progress on main app

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 import tensorflow as tf 
 import os 
+import cv2
+import imghdr
 
 # Avoid out of memory errors by setting GPU memory consumption growth
-gpus = tf.config.experimental.list_physical_devices('CPU')
+gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 print(gpus)
+
+data_dir = "data"
+
+image_exts = ['jpeg', 'jpg', 'bmp', 'png']
+
+for image_class in ow.listdir(data_dir):
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try:
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in image_exts:
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e:
+            print('Issue with image {}'.format(image_path))
+            # os.remove(image_path)
+
+            
