86e405f46c11055529856e27b61d79c8d830b5e0
ConnorOMalley <william.omalley@umconnect.umt.edu>
2023-12-20 10:18:28 -0700

    fixed bug

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ data_iterator = data.as_numpy_iterator()
 
 batch = data_iterator.next()
 
-fig, ax = plt.subplots(ncols=4, figsize=20,20)
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
 for idx, img in enumerate(batch[0][:4]):
     ax[idx].imshow(img.astype(int))
     ax[idx].title.set_text(batch[1][idx])
